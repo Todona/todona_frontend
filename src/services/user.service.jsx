a68c84f889a54057373fb7679004deb3279b57f5
radsadorn <sutthiratphutho@gmail.com>
2021-04-16 22:21:56 +0700

    Update api url

--- src/services/user.service.jsx
+++ src/services/user.service.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://todona.herokuapp.com/api/tasks';
+const API_URL = 'https://backendtodona.herokuapp.com/api/tasks';
 
 class UserService {
     getAllTasks() {
