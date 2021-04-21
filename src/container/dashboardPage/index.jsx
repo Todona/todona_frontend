d12137743ec96fb7832c00cb5e11477d900deef7
radsadorn <sutthiratphutho@gmail.com>
2021-04-21 17:51:48 +0700

    Update dashboard

--- src/container/dashboardPage/index.jsx
+++ src/container/dashboardPage/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import './dashboard.css';
 
 import UserService from "../../services/user.service";
 import useFullPageLoader from "../../hooks/useFullPageLoader";
@@ -8,6 +9,8 @@ const TodoPage = () => {
   const history = useHistory();
   const [todoCount, setTodoCount] = useState(0);
   const [doneCount, setDoneCount] = useState(0);
+  const [allCount, setAllCount] = useState(0);
+  const [percents, setPercents] = useState(0);
   
   const [loader, showLoader, hideLoader] = useFullPageLoader();
 
@@ -17,6 +20,11 @@ const TodoPage = () => {
     setTodoCount(todo.data.length);
     const done = await UserService.getDoneTasks();
     setDoneCount(done.data.length);
+
+    const p = Math.round(done.data.length/(todo.data.length + done.data.length) * 10000) / 100;
+
+    setPercents(p);
+    setAllCount(done.data.length + todo.data.length);
     hideLoader();
   };
 
@@ -24,21 +32,57 @@ const TodoPage = () => {
     fetchData();
   }, []);
 
-  const onDone = async (id) => {
+  const onDone = async () => {
     history.push(`/done`);
   }
 
-  const onTodo = async (id) => {
+  const onTodo = async () => {
     history.push(`/todo`);
   }
 
   return (
     <div className="DashboardPage">
+        <br />
         <h1 style={{ fontSize: "48px", margin: "0.5rem 0", color: "white" }}>
         Dashboard
         </h1>
-      <hr /> <br /> <br />  
-      {loader}
+        <hr /> <br /> <br />  
+        {loader}
+
+        <div className="progress-board">
+            <br />
+            <div className="progress">
+                <h2>YOUR PROGRESS : </h2> 
+                <h1>{percents} %</h1> 
+                <h3>FROM {allCount} TASKS</h3>
+            </div>
+            <br /> <br />
+            <div className="parent">
+                <div className="column">
+                    <div className="card" onClick={onTodo}>
+                        <h2>
+                            Todo Tasks :
+                        </h2>
+                        <h1>{todoCount}</h1>
+                    </div>
+                    <div className="detail">
+                        <h3>VIEW TODO TASKS</h3>
+                    </div>
+                </div>
+                
+                <div className="column">
+                    <div className="card" onClick={onDone}>
+                        <h2>
+                            Done Tasks :
+                        </h2>
+                        <h1>{doneCount}</h1>
+                    </div>
+                    <div className="detail">
+                        <h3>VIEW DONE TASKS</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
--- src/container/registerPage/index.jsx
+++ src/container/registerPage/index.jsx
@@ -115,6 +115,7 @@ export default class Register extends Component {
     this.form.validateAll();
 
     if (this.checkBtn.context._errors.length === 0) {
+      this.setState({ loading: true }) ;
       AuthService.register(
         this.state.username,
         this.state.email,
@@ -140,7 +141,8 @@ export default class Register extends Component {
 
           this.setState({
             successful: false,
-            message: resMessage
+            message: resMessage,
+            loading: false
           });
         }
       );
