005baf6178de14d68ad55794cbb858f16b4c65ef
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2023-03-07 15:07:56 +0700

    Update user.service.jsx

--- src/services/user.service.jsx
+++ src/services/user.service.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://backendtodona.herokuapp.com/api/tasks';
+const API_URL = 'https://maskvivid-thxx3o.stormkit.dev/api/tasks';
 //const API_URL = 'http://localhost:4000/api/tasks';
 
 class UserService {
