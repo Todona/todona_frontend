f71848b9dca643ea2c313225ae79463c2fe77122
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2023-03-07 16:52:51 +0700

    Update auth.service.jsx

--- src/services/auth.service.jsx
+++ src/services/auth.service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from './auth-header';
 
-const API_URL = process.env.API_URL + '/api/auth/';
+const API_URL = 'https://todona.stormkit.dev/api/auth/';
 //const API_URL = 'http://localhost:4000/api/auth/';
 
 class AuthService {
